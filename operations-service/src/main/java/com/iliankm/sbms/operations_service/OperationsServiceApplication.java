cf2a9cb8a21102f7a52258deaa1c83c4a046ad0c
Iliyan <iliyan.kamilov@mentormate.com>
2018-11-13 19:28:39 +0200

    initial configuration of spring boot apps

---
+++ operations-service/src/main/java/com/iliankm/sbms/operations_service/OperationsServiceApplication.java
@@ -0,0 +1,12 @@
+package com.iliankm.sbms.operations_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OperationsServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OperationsServiceApplication.class, args);
+    }
+}
---
+++ subscriber-service/src/main/java/com/iliankm/sbms/subscriber_service/SubscriberServiceApplication.java
@@ -0,0 +1,12 @@
+package com.iliankm.sbms.subscriber_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SubscriberServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SubscriberServiceApplication.class, args);
+    }
+}
