e30d610be0a6836575a1fbdff83d775716189157
Iliyan <iliyan.kamilov@mentormate.com>
2018-11-23 19:08:27 +0200

    parser-service docker configuration

---
+++ parser-service/src/main/java/com/iliankm/sbms/parser_service/ParserServiceApplication.java
@@ -0,0 +1,12 @@
+package com.iliankm.sbms.parser_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ParserServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ParserServiceApplication.class, args);
+    }
+}
