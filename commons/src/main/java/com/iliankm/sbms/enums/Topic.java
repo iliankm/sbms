fe8a1f1e5980e617193ebd5ad3c880950f6b3660
Iliyan <iliyan.kamilov@mentormate.com>
2019-01-25 18:04:20 +0200

    KafkaSenderService

---
+++ commons/src/main/java/com/iliankm/sbms/enums/Topic.java
@@ -0,0 +1,24 @@
+package com.iliankm.sbms.enums;
+
+/**
+ * Enumeration for kafka topics
+ */
+public enum Topic {
+    
+    TEST(Names.TOPIC_TEST);
+    
+    private String topicName;
+    
+    Topic(String topicName) {
+        this.topicName = topicName;
+    }
+    
+    public String getTopicName() {
+        return topicName;
+    }
+    
+    public static interface Names {
+        String TOPIC_TEST = "test";
+    }
+    
+}
---
+++ commons/src/main/java/com/iliankm/sbms/service/KafkaSenderService.java
@@ -0,0 +1,60 @@
+package com.iliankm.sbms.service;
+
+import java.util.LinkedList;
+import java.util.List;
+import java.util.concurrent.ExecutionException;
+import org.apache.kafka.clients.producer.ProducerRecord;
+import org.apache.kafka.common.header.Header;
+import org.apache.kafka.common.header.internals.RecordHeader;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.kafka.support.KafkaHeaders;
+import org.springframework.kafka.support.SendResult;
+import org.springframework.stereotype.Service;
+import org.springframework.util.concurrent.ListenableFuture;
+import org.springframework.util.concurrent.ListenableFutureCallback;
+import com.iliankm.sbms.enums.Topic;
+import com.iliankm.sbms.utils.RequestAttributesUtil;
+
+@Service
+public class KafkaSenderService {
+    
+    private static final Logger log = LoggerFactory.getLogger(KafkaSenderService.class);
+    
+    private final KafkaTemplate<String, Object> kafkaTemplate;
+    
+    public KafkaSenderService(KafkaTemplate<String, Object> kafkaTemplate) {
+        this.kafkaTemplate = kafkaTemplate;
+    }
+    
+    public void send(Topic topic, Object payload) {
+        //prepare headers
+        List<Header> headers = new LinkedList<>();
+        headers.add(new RecordHeader(KafkaHeaders.CORRELATION_ID, RequestAttributesUtil.get(RequestAttributesUtil.CORRELATION_ID).toString().getBytes()));
+        headers.add(new RecordHeader(KafkaHeaders.TOPIC, topic.getTopicName().getBytes()));
+
+        ProducerRecord<String, Object> producerRecord = new ProducerRecord<>(topic.getTopicName(), null, null, null, payload, headers);
+
+        ListenableFuture<SendResult<String, Object>> future = kafkaTemplate.send(producerRecord);
+
+        future.addCallback(new ListenableFutureCallback<SendResult<String, Object>>() {
+            @Override
+            public void onSuccess(SendResult<String, Object> result) {
+                log.info("Kafka message sent to topic {}", topic.getTopicName());
+            }
+
+            @Override
+            public void onFailure(Throwable ex) {
+                log.error("Error sending kafka message to topic " + topic.getTopicName(), ex);
+            }
+        });
+        
+        try {
+            future.get();
+        } catch (InterruptedException | ExecutionException e) {
+            throw new RuntimeException("Unable to send message to topic " + topic.getTopicName(), e);
+        }        
+    }
+
+}
