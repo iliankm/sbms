6f45e32317b973af434f1375a9857fef978c112d
Iliyan <iliyan.kamilov@mentormate.com>
2019-01-29 13:15:25 +0200

    KafkaSenderServiceTest

--- commons/src/test/java/com/iliankm/sbms/service/KafkaSenderServiceTest.java
+++ commons/src/test/java/com/iliankm/sbms/service/KafkaSenderServiceTest.java
@@ -1,8 +1,16 @@
 package com.iliankm.sbms.service;
 
 import static org.mockito.ArgumentMatchers.any;
-import static org.mockito.Mockito.when;
+import static org.mockito.Mockito.*;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.UUID;
+import java.util.concurrent.ExecutionException;
 import org.apache.kafka.clients.producer.ProducerRecord;
+import org.apache.kafka.common.header.Header;
+import org.apache.kafka.common.header.internals.RecordHeader;
+import org.junit.Test;
+import org.junit.runner.RunWith;
 import org.mockito.Mockito;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
@@ -10,23 +18,32 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Import;
 import org.springframework.context.annotation.Profile;
 import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.kafka.support.KafkaHeaders;
 import org.springframework.kafka.support.SendResult;
 import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.util.concurrent.ListenableFuture;
 import com.iliankm.sbms.config.ApplicationTestConfig;
 import com.iliankm.sbms.enums.Topic;
 import com.iliankm.sbms.service.KafkaSenderService;
 import com.iliankm.sbms.utils.RequestAttributesUtil;
 
-//@RunWith(SpringRunner.class)
+@RunWith(SpringRunner.class)
 @ActiveProfiles({"test"})
 public class KafkaSenderServiceTest {
+    
+    private static final String PAYLOAD = UUID.randomUUID().toString();
+    
+    private static final String CORRELATION_ID = UUID.randomUUID().toString(); 
     
     @Autowired
     private KafkaSenderService kafkaSenderService;
     
     @Autowired
     private KafkaTemplate<String, Object> kafkaTemplate;
+    
+    @Autowired
+    private RequestAttributesUtil requestAttributesUtil;
     
     @Profile({"test"})
     @Configuration
@@ -38,25 +55,34 @@ public class KafkaSenderServiceTest {
         public KafkaTemplate<String, Object> kafkaTemplate() {
             return Mockito.mock(KafkaTemplate.class);
         }
-        
         @Bean
-        public KafkaSenderService kafkaSenderService(RequestAttributesUtil requestAttributesUtil) {
-            return new KafkaSenderService(kafkaTemplate(), requestAttributesUtil);
+        public RequestAttributesUtil requestAttributesUtil() {
+            return Mockito.mock(RequestAttributesUtil.class);
         }
-       
+        @Bean
+        public KafkaSenderService kafkaSenderService() {
+            return new KafkaSenderService(kafkaTemplate(), requestAttributesUtil());
+        }
     }
     
     @SuppressWarnings("unchecked")
-    //@Test
-    public void kafkaSenderService_send() {
+    @Test
+    public void kafkaSenderService_send() throws InterruptedException, ExecutionException {
         //given
-        String payload = "";
         ListenableFuture<SendResult<String, Object>> future = Mockito.mock(ListenableFuture.class);
         when(kafkaTemplate.send(any(ProducerRecord.class))).thenReturn(future);
+        when(requestAttributesUtil.get(eq(RequestAttributesUtil.CORRELATION_ID))).thenReturn(CORRELATION_ID);
         //when
-        kafkaSenderService.send(Topic.TEST, payload);
+        kafkaSenderService.send(Topic.TEST, PAYLOAD);
         //then
-        //verify()
+        //verify kafkaTemplate.send was called with correct argument
+        List<Header> headers = new LinkedList<>();
+        headers.add(new RecordHeader(KafkaHeaders.CORRELATION_ID, CORRELATION_ID.getBytes()));
+        headers.add(new RecordHeader(KafkaHeaders.TOPIC, Topic.TEST.getTopicName().getBytes()));
+        ProducerRecord<String, Object> producerRecord = new ProducerRecord<>(Topic.TEST.getTopicName(), null, null, null, PAYLOAD, headers);
+        verify(kafkaTemplate, times(1)).send(eq(producerRecord));
+        //verify the get() method of the Future returned by kafkaTemplate.send was invoked
+        verify(future, times(1)).get();
     }
 
 }
