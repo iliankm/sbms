6169de13c184fb57c89f64539f96b908cacd8eaf
iliankm <ilian.kamilov@gmail.com>
2019-09-26 11:27:01 +0300

    code clean

--- commons/src/main/java/com/iliankm/sbms/jwt/JwtUtils.java
+++ commons/src/main/java/com/iliankm/sbms/jwt/JwtUtils.java
@@ -1,18 +1,18 @@
 package com.iliankm.sbms.jwt;
 
-import java.time.Instant;
-import java.time.LocalDateTime;
-import java.time.ZoneId;
-import java.util.Collections;
-import java.util.Date;
-import java.util.HashSet;
-import java.util.Set;
-import org.springframework.stereotype.Component;
 import com.auth0.jwt.JWT;
 import com.auth0.jwt.algorithms.Algorithm;
 import com.auth0.jwt.interfaces.Claim;
 import com.auth0.jwt.interfaces.DecodedJWT;
 import com.iliankm.sbms.utils.AppProperties;
+import org.springframework.stereotype.Component;
+
+import java.time.Instant;
+import java.time.LocalDateTime;
+import java.time.ZoneId;
+import java.util.Collections;
+import java.util.Date;
+import java.util.Set;
 
 @Component
 public class JwtUtils {
@@ -22,7 +22,6 @@ public class JwtUtils {
     private static final String CLAIM_IS_REFRESH_TOKEN = "CLAIM_IS_REFRESH_TOKEN";
     
     private final AppProperties applicationProperties;
-    
     private final Algorithm algorithm;
     
     public JwtUtils(AppProperties applicationProperties) {
@@ -33,17 +32,15 @@ public class JwtUtils {
     public String createAccessToken(String subject, Set<String> roles) {
         
         try {
-            String token = JWT.create()
-                            .withIssuer(ISSUER)
-                            .withNotBefore(Date.from(Instant.now()))
-                            .withExpiresAt(Date.from(LocalDateTime.now()
-                                            .plusMinutes(applicationProperties.jwtAccessTokenExpirationTime())
-                                            .atZone(ZoneId.systemDefault()).toInstant()))
-                            .withSubject(subject)
-                            .withArrayClaim(CLAIM_ROLES, roles.toArray(new String[roles.size()]))
-                            .sign(algorithm);
-            
-            return token;
+            return JWT.create()
+                    .withIssuer(ISSUER)
+                    .withNotBefore(Date.from(Instant.now()))
+                    .withExpiresAt(Date.from(LocalDateTime.now()
+                            .plusMinutes(applicationProperties.jwtAccessTokenExpirationTime())
+                            .atZone(ZoneId.systemDefault()).toInstant()))
+                    .withSubject(subject)
+                    .withArrayClaim(CLAIM_ROLES, roles.toArray(new String[0]))
+                    .sign(algorithm);
         } catch (Exception e) {
             throw new RuntimeException(e);
         }        
@@ -52,7 +49,7 @@ public class JwtUtils {
     public String createRefreshToken(String subject) {
 
         try {
-            String token = JWT.create()
+            return JWT.create()
                             .withIssuer(ISSUER)
                             .withNotBefore(Date.from(Instant.now()))
                             .withExpiresAt(Date.from(LocalDateTime.now()
@@ -61,8 +58,6 @@ public class JwtUtils {
                             .withSubject(subject)
                             .withClaim(CLAIM_IS_REFRESH_TOKEN, Boolean.TRUE)
                             .sign(algorithm);
-            
-            return token;
         } catch (Exception e) {
             throw new RuntimeException(e);
         }        
@@ -75,12 +70,11 @@ public class JwtUtils {
     
     public Set<String> getRoles(DecodedJWT decodedJWT) {
         Claim claim = decodedJWT.getClaim(CLAIM_ROLES);
-        return !claim.isNull() ? Collections.unmodifiableSet(new HashSet<String>(claim.asList(String.class))) : Collections.emptySet();
+        return !claim.isNull() ? Set.copyOf(claim.asList(String.class)) : Collections.emptySet();
     }
     
     public boolean isRefreshToken(DecodedJWT decodedJWT) {
         Claim claim = decodedJWT.getClaim(CLAIM_IS_REFRESH_TOKEN);
-        return !claim.isNull() ? claim.asBoolean().booleanValue() : false;
+        return !claim.isNull() ? claim.asBoolean() : false;
     }
-
 }
