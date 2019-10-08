081d36656e07148789047d9c286b11c24321c0f3
iliankm <ilian.kamilov@gmail.com>
2019-10-08 16:21:46 +0300

    kafka transactional configuration additional test for the transactional sender/consumer

--- commons/src/test/java/com/iliankm/sbms/consumer/TestTransactionalConsumerTest.java
+++ commons/src/test/java/com/iliankm/sbms/consumer/TestTransactionalConsumerTest.java
@@ -41,6 +41,10 @@ public class TestTransactionalConsumerTest {
     static {
         MESSAGE.put(UUID.randomUUID().toString(), UUID.randomUUID().toString());
     }
+    private static final Map<String, String> MESSAGE_2 = new HashMap<>();
+    static {
+        MESSAGE_2.put(UUID.randomUUID().toString(), UUID.randomUUID().toString());
+    }
     private static final Map<String, String> MESSAGE_OK = new HashMap<>();
     static {
         MESSAGE_OK.put(UUID.randomUUID().toString(), UUID.randomUUID().toString());
@@ -91,38 +95,54 @@ public class TestTransactionalConsumerTest {
     public void setup() throws InterruptedException {
         RequestAttributesUtil.setCorrelationId(CORRELATION_ID);
         //give time EMBEDDED_KAFKA to start
-        Thread.sleep(1000);
+        Thread.sleep(10000);
     }
     
     @Test
     public void send_to_Transactional_TopicA_Expect_Fail() throws InterruptedException {
         //given
-        CountDownLatch consumerLatch = new CountDownLatch(1);
+        CountDownLatch latch = new CountDownLatch(1);
         doAnswer(i -> {
-            consumerLatch.countDown();
+            latch.countDown();
             return null;
-        }).when(testTransactionalTopicBService).process(any());
+        }).when(testTransactionalTopicBService).process(eq(MESSAGE));
         //when
         kafkaSenderService.sendInLocalTransaction(Pair.of(Topic.TRANSACTIONAL_A, MESSAGE));
-        consumerLatch.await(5, TimeUnit.SECONDS);
+        latch.await(5, TimeUnit.SECONDS);
         //then
-        assertEquals(1, consumerLatch.getCount());
-        verify(testTransactionalTopicBService, times(0)).process(any());
+        assertEquals(1, latch.getCount());
+        verify(testTransactionalTopicBService, times(0)).process(eq(MESSAGE));
+    }
+
+    @Test
+    public void send_to_Transactional_TopicB_Expect_Fail() throws InterruptedException {
+        //given
+        CountDownLatch latch = new CountDownLatch(3);
+        doAnswer(i -> {
+            latch.countDown();
+            throw new RuntimeException("Simulated runtime exception...");
+        }).when(testTransactionalTopicBService).process(eq(MESSAGE_2));
+        //when
+        kafkaSenderService.sendInLocalTransaction(Pair.of(Topic.TRANSACTIONAL_B, MESSAGE_2));
+        latch.await(5, TimeUnit.SECONDS);
+        //then
+        assertEquals(0, latch.getCount());
+        verify(testTransactionalTopicBService, times(3)).process(eq(MESSAGE_2));
     }
 
     @Test
     public void send_to_Transactional_TopicA_Expect_OK() throws InterruptedException {
         //given
-        CountDownLatch consumerLatch = new CountDownLatch(1);
+        CountDownLatch latch = new CountDownLatch(1);
         doAnswer(i -> {
-            consumerLatch.countDown();
+            latch.countDown();
             return null;
-        }).when(testTransactionalTopicBService).process(any());
+        }).when(testTransactionalTopicBService).process(eq(MESSAGE_OK));
         //when
         kafkaSenderService.sendInLocalTransaction(Pair.of(Topic.TRANSACTIONAL_A, MESSAGE_OK));
-        consumerLatch.await(5, TimeUnit.SECONDS);
+        latch.await(5, TimeUnit.SECONDS);
         //then
-        assertEquals(0, consumerLatch.getCount());
+        assertEquals(0, latch.getCount());
         verify(testTransactionalTopicBService, times(1)).process(eq(MESSAGE_OK));
     }
 }
