a77203a0228ac1caa1187bc8f0caadc527aecc7c
iliankm <ilian.kamilov@gmail.com>
2019-10-10 17:21:29 +0300

    code clean

--- commons/src/main/java/com/iliankm/sbms/config/SwaggerConfig.java
+++ commons/src/main/java/com/iliankm/sbms/config/SwaggerConfig.java
@@ -1,18 +1,19 @@
 package com.iliankm.sbms.config;
 
-import java.util.Arrays;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpHeaders;
 import springfox.documentation.builders.PathSelectors;
-import springfox.documentation.spi.DocumentationType;
-import springfox.documentation.spi.service.contexts.SecurityContext;
-import springfox.documentation.spring.web.plugins.Docket;
-import springfox.documentation.swagger2.annotations.EnableSwagger2;
 import springfox.documentation.builders.RequestHandlerSelectors;
 import springfox.documentation.service.ApiKey;
 import springfox.documentation.service.AuthorizationScope;
 import springfox.documentation.service.SecurityReference;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spi.service.contexts.SecurityContext;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+import java.util.List;
 
 @Configuration
 @EnableSwagger2
@@ -20,8 +21,8 @@ public class SwaggerConfig {
     @Bean
     public Docket api() {
         return new Docket(DocumentationType.SWAGGER_2)
-            .securitySchemes(Arrays.asList(new ApiKey("jwt", HttpHeaders.AUTHORIZATION, "header")))
-            .securityContexts(Arrays.asList(securityContext()))
+            .securitySchemes(List.of(new ApiKey("jwt", HttpHeaders.AUTHORIZATION, "header")))
+            .securityContexts(List.of(securityContext()))
             .select()
             .apis(RequestHandlerSelectors.basePackage("com.iliankm.sbms"))
             .paths(PathSelectors.any())
@@ -31,16 +32,15 @@ public class SwaggerConfig {
     
     private SecurityContext securityContext() {
         return SecurityContext.builder()
-                        .securityReferences(Arrays.asList(new SecurityReference("jwt", scopes())))
+                        .securityReferences(List.of(new SecurityReference("jwt", scopes())))
                         .forPaths(PathSelectors.regex("/api.*")).build();
     }
-    
+
     private AuthorizationScope[] scopes() {
-        AuthorizationScope[] scopes = {
-                        new AuthorizationScope("read", "for read operations"),
-                        new AuthorizationScope("write", "for write operations"),
-                        new AuthorizationScope("foo", "Access foo API")};
-        return scopes;
+        return new AuthorizationScope[] {
+                new AuthorizationScope("read", "for read operations"),
+                new AuthorizationScope("write", "for write operations"),
+                new AuthorizationScope("foo", "Access foo API")
+        };
     }
-    
 }
