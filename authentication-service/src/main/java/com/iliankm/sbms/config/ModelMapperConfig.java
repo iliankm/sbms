2f2410516fcd64955340f0cc3e7b443f621469c3
iliankm <ilian.kamilov@gmail.com>
2019-10-14 11:10:13 +0300

    ModelMapper

---
+++ authentication-service/src/main/java/com/iliankm/sbms/config/ModelMapperConfig.java
@@ -0,0 +1,17 @@
+package com.iliankm.sbms.config;
+
+import org.modelmapper.ModelMapper;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class ModelMapperConfig {
+    @Bean
+    public ModelMapper modelMapper() {
+        ModelMapper modelMapper = new ModelMapper();
+
+        //typeMap configurations goes here...
+
+        return modelMapper;
+    }
+}
